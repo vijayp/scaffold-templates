bf723404ef026aca2393b72f1a36e829102b26f3
vijayp <vijayp@token.local>
2021-02-05 18:22:29 -0500

    fix default host to listen on

--- py/____PROGRAM_webserver.py
+++ py/____PROGRAM_webserver.py
@@ -3,6 +3,7 @@
 import ____PROGRAM_lib 
 import logging
 import os
+import argparse
 
 from flask import Flask
 from flask import render_template
@@ -32,11 +33,29 @@ def render():
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='____PROGRAM_webserver')
+    parser.add_argument('-p', '--port', required=False, type=int, help='port to listen to ', default=5000)
+    parser.add_argument('-a', '--host', required=False, type=str, help='address to listen on ', default='0.0.0.0')
+    parser.add_argument('-v', '--verbose', default=False, action="store_true",
+                        help="print details")
+    args = parser.parse_args()
+    logging.basicConfig(
+        level=logging.INFO if not args.verbose else logging.DEBUG,
+        format="%(asctime)s [%(levelname)s] %(message)s",
+        handlers=[
+            logging.FileHandler("debug.log"),
+            logging.StreamHandler()
+        ]
+    )
+
+    logging.info('starting up!')
+
+
     # this is used for CSRF and other related security matters
     SECRET_KEY = os.environ.get('SECRET_KEY')
     if not SECRET_KEY:
         logging.warn('Using random key for forms!')
         SECRET_KEY = os.urandom(24)
     app.config['SECRET_KEY'] = SECRET_KEY
-    app.run()
+    app.run(port=args.port, host=args.host)
 
